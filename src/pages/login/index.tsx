4d2a90e202409f235beb92c6984cda360113bab0
Bhavesh Patil <bhaveshpatil918@gmail.com>
2023-08-15 14:11:11 +0530

    Added Register and Login Page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,61 @@
-import React from "react";
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import { signIn } from "next-auth/react";
+import React, { type FormEvent } from "react";
 
-const Login = (props) => {
-  return <div>index</div>;
+const Index = (props: never) => {
+  async function handleFormSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    const form = e.target as HTMLFormElement;
+    const data = new FormData(form);
+
+    const email = data.get("username");
+    const password = data.get("password");
+    await signIn("credentials", {
+      email: email,
+      password: password,
+      callbackUrl: "/",
+    });
+    console.log(email, password);
+  }
+  return (
+    <div className="flex h-screen items-center justify-center bg-base-300">
+      <form
+        onSubmit={handleFormSubmit}
+        action=""
+        className="form-control w-full max-w-xs flex-col gap-5 rounded-lg bg-base-100 p-10"
+      >
+        <div className="form-control w-full max-w-xs ">
+          <label className="label" htmlFor="username">
+            <span className="label-text">Username</span>
+          </label>
+          <input
+            type="text"
+            name="username"
+            placeholder="Enter your username"
+            className="input input-bordered w-full max-w-xs"
+          />
+        </div>
+        <div className="form-control w-full max-w-xs ">
+          <label className="label" htmlFor="password">
+            <span className="label-text">Password</span>
+          </label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter your Password"
+            className="input input-bordered w-full max-w-xs"
+          />
+        </div>
+        <button
+          type="submit"
+          className="rounded-full bg-primary py-3 text-primary-content focus:bg-primary-focus"
+        >
+          Login
+        </button>
+      </form>
+    </div>
+  );
 };
 
-export default Login;
+export default Index;
---
+++ src/pages/register/index.tsx
@@ -0,0 +1,110 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import { User } from "@prisma/client";
+import { signIn } from "next-auth/react";
+import React, { type FormEvent } from "react";
+
+const Register = (props: unknown) => {
+  const [errors, setErrors] = React.useState();
+
+  async function handleFormSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    const form = e.target as HTMLFormElement;
+    const data = new FormData(form);
+
+    const email = data.get("mail");
+    const username = data.get("username");
+    const password = data.get("password");
+    const confirmPassword = data.get("confirm-password");
+
+    const res = await fetch("/api/auth/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        username,
+        password,
+        confirmPassword,
+      }),
+    });
+    const userInfo = await res.json();
+    if (res.status === 200) {
+      await signIn("credentials", {
+        email,
+        password,
+        callbackUrl: "/",
+      });
+    } else {
+      setErrors(userInfo);
+    }
+  }
+
+  return (
+    <div className="flex h-screen items-center justify-center bg-base-300">
+      <form
+        onSubmit={handleFormSubmit}
+        action=""
+        className="form-control w-full max-w-xs flex-col gap-5 rounded-lg bg-base-100 p-10"
+      >
+        <div className="form-control w-full max-w-xs ">
+          <label className="label" htmlFor="mail">
+            <span className="label-text">E-Mail</span>
+          </label>
+          <input
+            type="text"
+            name="mail"
+            placeholder="Enter your E-Mail"
+            className="input input-bordered w-full max-w-xs"
+          />
+        </div>
+        <div className="form-control w-full max-w-xs ">
+          <label className="label" htmlFor="username">
+            <span className="label-text">Username</span>
+          </label>
+          <input
+            type="text"
+            name="username"
+            placeholder="Enter your username"
+            className="input input-bordered w-full max-w-xs"
+          />
+        </div>
+        <div className="form-control w-full max-w-xs ">
+          <label className="label" htmlFor="password">
+            <span className="label-text">Password</span>
+          </label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter your Password"
+            className="input input-bordered w-full max-w-xs"
+          />
+        </div>
+        <div className="form-control w-full max-w-xs ">
+          <label className="label" htmlFor="password">
+            <span className="label-text">Confirm Password</span>
+          </label>
+          <input
+            type="password"
+            name="confirm-password"
+            placeholder="Enter your Confirm Password"
+            className="input input-bordered w-full max-w-xs"
+          />
+        </div>
+        <p className="text-red-500">{errors?.error as string}</p>
+        <button
+          type="submit"
+          className="rounded-full bg-primary py-3 text-primary-content focus:bg-primary-focus"
+        >
+          Register
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
