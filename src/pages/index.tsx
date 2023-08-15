f06c6a67ff7a5d1180b4cd678bbc2f8842edcc7f
Bhavesh Patil <bhaveshpatil918@gmail.com>
2023-08-15 15:52:36 +0530

    Hoem pages props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
 import { api } from "@/utils/api";
 
-export default function Home() {
+export default function Home(props) {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
   return (
@@ -57,6 +58,8 @@ export default function Home() {
 function AuthShowcase() {
   const { data: sessionData } = useSession();
 
+  console.log(sessionData);
+
   const { data: secretMessage } = api.example.getSecretMessage.useQuery(
     undefined, // no input
     { enabled: sessionData?.user !== undefined }
@@ -65,7 +68,7 @@ function AuthShowcase() {
   return (
     <div className="flex flex-col items-center justify-center gap-4">
       <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
+        {sessionData && <span>Logged in as {sessionData.user?.username}</span>}
         {secretMessage && <span> - {secretMessage}</span>}
       </p>
       <button
