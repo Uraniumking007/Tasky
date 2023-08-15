acfa5fadb08d57c761d4f2fcb36849b09044e039
Bhavesh Patil <bhaveshpatil918@gmail.com>
2023-08-15 18:34:32 +0530

    Added Defaults Meta data

---
+++ src/pages/_document.tsx
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document(props) {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta
+          name="One of the best Task Manager in Creation."
+          content="Created by Bhavesh Patil : bhaveshp.dev"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
