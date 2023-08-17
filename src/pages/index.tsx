415afd4875f661913f16b09d88d0e5b210ca0340
Bhavesh Patil <bhaveshpatil918@gmail.com>
2023-08-17 15:35:13 +0530

    Home Page
    - Added Get Started Option
    - Simple UI
    - Added Animations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,8 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
 import { api } from "@/utils/api";
-import Navbar from "@/components/Navbar";
+
+import { Player } from "@lottiefiles/react-lottie-player";
 
 export default function Home(props) {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
@@ -13,16 +14,28 @@ export default function Home(props) {
       <Head>
         <title>Tasky</title>
       </Head>
-      {/* <Navbar /> */}
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-base-100 to-base-300">
-        <AuthShowcase />
-        <div className="flex flex-col items-center justify-center gap-4">
-          <Link href="/tasks">
-            <p className="rounded-full bg-base-100 px-10 py-3 font-semibold text-base-content no-underline transition hover:bg-base-200">
-              Tasks
+      <main className="flex min-h-screen flex-col items-center justify-center overflow-auto bg-gradient-to-b from-base-100 to-base-300">
+        <div className="flex items-center justify-center gap-4">
+          <div className="flex flex-col items-center justify-center gap-4 text-center">
+            <h1 className="text-4xl font-bold">Tasky</h1>
+            <p className="w-1/2 text-base sm:text-xl">
+              A simple task manager for your everyday tasks be they may perosnal
+              or corporate.
             </p>
-          </Link>
-          <Link href="/about"></Link>
+            <button className="btn rounded-full bg-base-100 px-10 py-3 font-semibold normal-case text-base-content no-underline shadow-sm transition hover:bg-base-200">
+              <Link href={"/tasks"}>Get Started</Link>
+            </button>
+          </div>
+          <Player
+            autoplay
+            loop={true}
+            direction={1}
+            controls={false}
+            speed={1}
+            keepLastFrame
+            src="/animations/tasks.json"
+            className="hidden md:block"
+          />
         </div>
       </main>
     </>
