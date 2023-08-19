99f25607da03d3065c72d50e2d4c8a2a0fd68192
Bhavesh Patil <96092183+Uraniumking007@users.noreply.github.com>
2023-08-19 19:00:54 +0530

    Added Task Router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { exampleRouter } from "@/server/api/routers/example";
 import { createTRPCRouter } from "@/server/api/trpc";
+import { TaskRouter } from "./routers/tasks";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +9,7 @@ import { createTRPCRouter } from "@/server/api/trpc";
  */
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
+  tasks: TaskRouter,
 });
 
 // export type definition of API
