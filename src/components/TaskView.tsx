f197a962fe8cdcd5796e0bed1e6995be53900c0a
Bhavesh Patil <96092183+Uraniumking007@users.noreply.github.com>
2023-08-19 20:19:51 +0530

    Added Description and Priority Badges

--- src/components/TaskView.tsx
+++ src/components/TaskView.tsx
@@ -2,14 +2,19 @@ import type { Tasks } from "@prisma/client";
 
 function TasksView({ data }: { data: unknown[] }) {
   return (
-    <div className="flex h-fit w-full flex-col items-center gap-5">
+    <div className="items flex h-fit w-full flex-col gap-5 pl-16">
       {data?.map((task: Tasks) => {
         return (
-          <div
-            key={task.id}
-            className="flex h-fit w-full flex-col items-center gap-5"
-          >
-            {task.Title}
+          <div key={task.id} className="flex flex-col gap-1">
+            <div className="link-primary link text-lg hover:link-hover">
+              {task.Title}
+            </div>
+            <div className="link-primary link text-sm hover:link-hover">
+              {task?.Description}
+            </div>
+            <div className="badge badge-primary badge-outline">
+              {task.Priority}
+            </div>
           </div>
         );
       })}
