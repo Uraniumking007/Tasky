28d7b13b79ea2986d3013a81aed7916546f301ed
Bhavesh Patil <96092183+Uraniumking007@users.noreply.github.com>
2023-08-19 19:00:41 +0530

    Created Tasks Page

--- src/pages/tasks/index.tsx
+++ src/pages/tasks/index.tsx
@@ -1,24 +1,71 @@
-import React from "react";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useSession } from "next-auth/react";
+import React, { type FormEvent, useEffect } from "react";
+import { useRouter } from "next/router";
+import { api } from "@/utils/api";
+import { Tasks } from "@prisma/client";
+import Loading from "@/components/Loading";
+import TasksView from "@/components/TaskView";
 
 const Index = (props) => {
+  const { data: SessionData, status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!SessionData?.user && status !== "loading") {
+      void router.push("/login");
+    }
+  }, [SessionData?.user, router, status]);
+
+  const { data } = api.tasks.getAll.useQuery(undefined, {
+    enabled: !!SessionData?.user,
+  });
+
+  const ctx = api.useContext();
+
+  const { mutate, isLoading } = api.tasks.createTasky.useMutation({
+    onSuccess: () => {
+      void ctx.tasks.getAll.invalidate();
+    },
+  });
+
+  function addTask(e: FormEvent) {
+    e.preventDefault();
+    const form = e.target as HTMLFormElement;
+    const data = new FormData(form);
+    const title = data.get("title") as string;
+    const description = data.get("description") as string | null;
+    const categories = (data.get("categories") as unknown as string[]) || [];
+    mutate({ title, description, categories });
+    form.reset();
+  }
+
   return (
     <div className="mt-5 flex min-h-screen flex-col items-center gap-5">
       <p className="text-center text-2xl font-bold">Manage Tasks</p>
-      <form className="flex w-full flex-col items-center">
+      <form
+        className="flex w-full flex-col items-center"
+        onSubmit={addTask}
+        action=""
+      >
         <div className="form-control flex h-fit w-full items-center gap-3">
           <input
             type="text"
             placeholder="Type here"
+            name="title"
+            disabled={isLoading}
             className="input input-bordered w-11/12 "
           />
           <button
             type="submit"
-            className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg"
+            disabled={isLoading}
+            className="btn flex items-center justify-center px-10 py-5 align-middle sm:btn-md md:btn-md lg:btn-lg"
           >
             Add
           </button>
         </div>
       </form>
+      {data ? <TasksView data={data} /> : <Loading />}
     </div>
   );
 };
