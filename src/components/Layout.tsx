702dce2a6d775cd531cb9337301360a2d8e4ebc9
Bhavesh Patil <96092183+Uraniumking007@users.noreply.github.com>
2023-08-21 09:46:59 +0530

    Added PerPage Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,9 +5,12 @@ import Navbar from "./Navbar";
 export default function Layout({ children }) {
   const [theme, setTheme] = useAtom(themeAtom);
   return (
-    <div data-theme={theme == "fantasy" ? "fantasy" : "night"}>
+    <div
+      className="h-screen w-screen"
+      data-theme={theme == "fantasy" ? "fantasy" : "night"}
+    >
       <Navbar />
-      <main>{children}</main>
+      {children}
     </div>
   );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,27 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-import { type AppType } from "next/app";
+import App, { type AppType } from "next/app";
 import { api } from "@/utils/api";
 import "@/styles/globals.css";
-import Layout from "@/components/Layout";
+import type { ReactElement, ReactNode } from "react";
+import type { NextPage } from "next";
+
+export type NextPageWithLayout<P = object, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
+}: {
+  Component: NextPageWithLayout;
+  pageProps: { session: Session | null };
 }) => {
+  const getLayout = Component.getLayout ?? ((page) => page);
+
   return (
     <SessionProvider session={session}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      {getLayout(<Component {...pageProps} />)}{" "}
     </SessionProvider>
   );
 };
