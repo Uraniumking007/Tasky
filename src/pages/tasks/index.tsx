d97151d301aca1f69e00c38dc03d45d92a548142
Bhavesh Patil <96092183+Uraniumking007@users.noreply.github.com>
2023-08-31 22:56:30 +0530

    Added Date Input

--- src/pages/tasks/index.tsx
+++ src/pages/tasks/index.tsx
@@ -8,6 +8,7 @@ import Loading from "@/components/Loading";
 import TasksView from "@/components/TaskView";
 import Layout from "@/components/Layout";
 import { MultiSelect } from "@mantine/core";
+import { DateInput } from "@mantine/dates";
 
 const options: { label: string; value: string }[] = [
   "Personal",
@@ -23,6 +24,7 @@ const Index = (props) => {
   const { data: SessionData, status } = useSession();
   const [categories, setCategories] = useState<(typeof options)[number][]>([]);
   const router = useRouter();
+  const [date, setDate] = useState<Date | null>(null);
 
   useEffect(() => {
     if (!SessionData?.user && status !== "loading") {
@@ -48,7 +50,6 @@ const Index = (props) => {
     const data = new FormData(form);
     const title = data.get("title") as string;
     const description = data.get("description") as string | null;
-    const date = new Date(data.get("deadline") as string);
     const priority = data.get("priority") as string;
     console.log(categories);
 
@@ -60,75 +61,72 @@ const Index = (props) => {
     <div className="mt-5 flex flex-col items-center gap-5">
       <p className="text-center text-2xl font-bold">Manage Tasks</p>
       <form
-        className="flex w-full flex-col items-center"
+        className="flex w-full flex-col items-center p-12"
         onSubmit={addTask}
         action=""
       >
-        <div className="form-control flex h-fit w-full items-center gap-3">
+        <div className="form-control flex h-fit w-full flex-row items-center gap-4">
           <input
             type="text"
             placeholder="Enter Task Here"
             name="title"
             disabled={isLoading}
-            className="input input-bordered w-11/12"
+            className="input input-bordered w-full"
           />
-          <select
-            name="priority"
-            disabled={isLoading}
-            defaultValue={"No Priority"}
-            className="select w-full max-w-xs"
-          >
-            <option disabled selected>
-              Priority
-            </option>
-            <option>No Priority</option>
-            <option>Low Priority</option>
-            <option>Medium Priority</option>
-            <option>High Priority</option>
-          </select>
-          <input
-            type="date"
-            name="deadline"
-            id="deadline"
-            className="bg-base-200 px-4 py-2 text-base-content"
-          />
-          <MultiSelect
-            label="Select Categories"
-            classNames={{
-              defaultValue: "select select-bordered select-sm",
-              input: "input input-bordered flex mr-4 w-full",
-              label: "text-base-content",
-              defaultValueLabel:
-                "flex justify-center items-center text-base-content",
-              values: "",
-              rightSection: "ml-4",
-            }}
-            data={options}
-            placeholder="Select items"
-            searchable
-            creatable
-            getCreateLabel={(query) => `+ Create ${query}`}
-            onCreate={(query) => {
-              const item = { value: query, label: query };
-              setCategories((current) => [...current, item]);
-              return item;
-            }}
-            onChange={(query) => {
-              setCategories(
-                query.map((item) => ({
-                  label: item,
-                  value: item,
-                }))
-              );
-            }}
-          />
-          <button
-            type="submit"
-            disabled={isLoading}
-            className="btn flex items-center justify-center px-10 py-5 align-middle sm:btn-md md:btn-md lg:btn-lg"
-          >
-            Add
-          </button>
+          <div className="flex w-5/12 flex-col gap-2">
+            <select
+              name="priority"
+              disabled={isLoading}
+              defaultValue={"No Priority"}
+              className="select w-full border border-base-content"
+            >
+              <option disabled selected>
+                Priority
+              </option>
+              <option>No Priority</option>
+              <option>Low Priority</option>
+              <option>Medium Priority</option>
+              <option>High Priority</option>
+            </select>
+            <DateInput value={date} onChange={setDate} />
+            <MultiSelect
+              label="Select Categories"
+              classNames={{
+                defaultValue: "select select-bordered select-sm",
+                input: "input input-bordered flex mr-4 w-full",
+                label: "text-base-content",
+                defaultValueLabel:
+                  "flex justify-center items-center text-base-content",
+                values: "",
+                rightSection: "ml-4",
+              }}
+              data={options}
+              placeholder="Select items"
+              searchable
+              creatable
+              getCreateLabel={(query) => `+ Create ${query}`}
+              onCreate={(query) => {
+                const item = { value: query, label: query };
+                setCategories((current) => [...current, item]);
+                return item;
+              }}
+              onChange={(query) => {
+                setCategories(
+                  query.map((item) => ({
+                    label: item,
+                    value: item,
+                  }))
+                );
+              }}
+            />
+            <button
+              type="submit"
+              disabled={isLoading}
+              className="btn flex items-center justify-center px-10 py-5 align-middle sm:btn-md md:btn-md lg:btn-lg"
+            >
+              Add
+            </button>
+          </div>
         </div>
       </form>
       {data ? <TasksView data={data} /> : <Loading />}
