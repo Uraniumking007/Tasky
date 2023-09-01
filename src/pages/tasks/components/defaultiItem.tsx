02c2af1c015580aa30e55775774b8e0639d58a76
Bhavesh Patil <bhaveshpatil918@gmail.com>
2023-09-01 10:36:19 +0530

    Custom Default Dropdown for MultiSelect

---
+++ src/pages/tasks/components/defaultiItem.tsx
@@ -0,0 +1,21 @@
+import React, { forwardRef } from "react";
+
+export interface SelectItemProps
+  extends Omit<React.ComponentPropsWithoutRef<"div">, "value"> {
+  label: React.ReactNode;
+  value?: string;
+}
+
+export const DefaultItem = forwardRef<HTMLDivElement, SelectItemProps>(
+  ({ label, value, ...others }: SelectItemProps, ref) => (
+    <div
+      ref={ref}
+      {...others}
+      className="rounded-md bg-base-100 px-4 py-1 text-base-content hover:bg-base-200"
+    >
+      {label ?? value}
+    </div>
+  )
+);
+
+DefaultItem.displayName = "@mantine/core/DefaultItem";
