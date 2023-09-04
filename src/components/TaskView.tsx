c68093152c97351b9013a7436c79cb830182eb97
Bhavesh Patil <96092183+Uraniumking007@users.noreply.github.com>
2023-09-04 09:19:35 +0530

    Added Edit and Delete Buttons

--- src/components/TaskView.tsx
+++ src/components/TaskView.tsx
@@ -1,19 +1,31 @@
 import type { Tasks } from "@prisma/client";
+import { Edit, Trash2 } from "lucide-react";
+import CircularButton from "./Button/circularButton";
 
 function TasksView({ data }: { data: Tasks[] }) {
   return (
     <div className="flex h-fit w-full flex-col gap-5 pl-16">
       {data?.map((task: Tasks) => {
         return (
-          <div key={task.id} className="flex flex-col gap-1">
-            <div className="link-primary link text-lg hover:link-hover">
-              {task.Title}
+          <div key={task.id} className="flex justify-between gap-1">
+            <div className="flex flex-col gap-1">
+              <div className="link-primary link text-xl font-bold hover:link-hover">
+                {task.Title}
+              </div>
+              <div className="text-sm hover:link-hover">
+                {task?.Description}
+              </div>
+              <div className="badge badge-primary badge-outline badge-xs mt-2 p-2">
+                {task.Priority}
+              </div>
             </div>
-            <div className="link-primary link text-sm hover:link-hover">
-              {task?.Description}
-            </div>
-            <div className="badge badge-primary badge-outline">
-              {task.Priority}
+            <div className="flex gap-2">
+              <CircularButton>
+                <Edit className="text-secondary-content" />
+              </CircularButton>
+              <CircularButton>
+                <Trash2 className="text-secondary-content" />
+              </CircularButton>
             </div>
           </div>
         );
