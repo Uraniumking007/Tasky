880c4308b594a36e144f6d3ddaf2e6367c91c880
Bhavesh Patil <96092183+Uraniumking007@users.noreply.github.com>
2023-09-18 02:06:58 +0530

    Added Theme Switcher

---
+++ src/components/Defaults/themeSwitcher.tsx
@@ -0,0 +1,45 @@
+// components/ThemeSwitcher.tsx
+import { Switch } from "@nextui-org/react";
+import { set } from "date-fns";
+import { MoonIcon, SunIcon } from "lucide-react";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+export const ThemeSwitcher = () => {
+  const { theme, setTheme } = useTheme();
+  const [isDark, setIsDark] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (theme === "dark") {
+      setIsDark(true);
+    } else {
+      setIsDark(false);
+    }
+  }, [theme]);
+
+  function handleThemeChange() {
+    if (isDark) {
+      setTheme("light");
+      setIsDark(false);
+    } else {
+      setTheme("dark");
+      setIsDark(true);
+    }
+  }
+
+  return (
+    <Switch
+      defaultSelected={isDark}
+      size="lg"
+      color="secondary"
+      thumbIcon={({ isSelected }) =>
+        isSelected ? <SunIcon className="text-black" /> : <MoonIcon />
+      }
+      onClick={handleThemeChange}
+    />
+    // <div>
+    //   <button onClick={() => setTheme("light")}>Light Mode</button>
+    //   <button onClick={() => setTheme("dark")}>Dark Mode</button>
+    // </div>
+  );
+};
