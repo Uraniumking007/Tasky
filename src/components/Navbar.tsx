13c80c1893553a937c4f6830fcd728bcd96992b1
Bhavesh Patil <96092183+Uraniumking007@users.noreply.github.com>
2023-09-18 02:06:45 +0530

    Recreation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,202 +1,77 @@
-/* eslint-disable @next/next/no-img-element */
-import { themeAtom } from "@/utils/atoms";
-import { useAtom } from "jotai";
+import React, { useState } from "react";
+
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  DropdownItem,
+  DropdownTrigger,
+  Dropdown,
+  DropdownMenu,
+  Avatar,
+  Button,
+  useDisclosure,
+  user,
+} from "@nextui-org/react";
+import { ThemeSwitcher } from "./Defaults/themeSwitcher";
 import { signOut, useSession } from "next-auth/react";
-import { Moon, Sun } from "lucide-react";
-import { Button } from "./ui/button";
-import Link from "next/link";
-import { useDisclosure } from "@nextui-org/react";
 import LoginModal from "./Modals/loginModal";
+import { User } from "@prisma/client";
+import { AdapterUser } from "next-auth/adapters";
+import { SessionUserTypes } from "@/utils/types";
+// import { AcmeLogo } from "./AcmeLogo.jsx";
 
-const Navbar = () => {
-  const [theme, setTheme] = useAtom(themeAtom);
-  const { data: session } = useSession();
+export default function App() {
+  const { data, status } = useSession();
+
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
-
-  function handleThemeChange() {
-    setTheme(theme === "fantasy" ? "night" : "fantasy");
+  if (status === "loading") {
+    return null;
   }
-
-  function handleLogout() {
-    void signOut({
-      callbackUrl: "/",
-    });
+  function handleSignOut() {
+    void signOut();
   }
 
   return (
-    <div className="navbar z-10 bg-base-100 drop-shadow-2xl">
-      <div className="navbar-start">
-        <div className="drawer lg:hidden">
-          <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-          <div className="drawer-content">
-            {/* Page content here */}
-            <label
-              htmlFor="my-drawer"
-              className="btn btn-primary drawer-button"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
-            </label>
-          </div>
-          <div className="drawer-side">
-            <label htmlFor="my-drawer" className="drawer-overlay"></label>
-            <div className="menu h-full w-80 justify-between bg-base-200 p-4 text-base-content">
-              <ul className="">
-                {/* Sidebar content here */}
-                <li>
-                  <a>Sidebar Item 1</a>
-                </li>
-                <li>
-                  <a>Sidebar Item 2</a>
-                </li>
-                <li className="drawer-toggle">Close</li>
-              </ul>
-              <div className="flex justify-end">
-                <input
-                  id="my-drawer"
-                  type="checkbox"
-                  className="drawer-toggle"
-                />
-                {/* Page content here */}
-                <label
-                  htmlFor="my-drawer"
-                  className="btn btn-primary drawer-button"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="h-6 w-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M15.75 19.5L8.25 12l7.5-7.5"
-                    />
-                  </svg>
-                </label>
-              </div>
-            </div>
-          </div>
-        </div>
-        {/* <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
+    <Navbar maxWidth="full">
+      <NavbarBrand>
+        <p className="font-bold text-inherit">TASKY</p>
+      </NavbarBrand>
+      <NavbarContent as="div" justify="end">
+        <ThemeSwitcher />
+        {data?.user ? (
+          <Dropdown placement="bottom-end">
+            <DropdownTrigger>
+              <Avatar
+                isBordered
+                as="button"
+                className="transition-transform"
+                color="secondary"
+                name="Jason Hughes"
+                size="sm"
+                src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
               />
-            </svg> */}
-        {/* </label> */}
-        {/* <ul
-            tabIndex={0}
-            className="menu dropdown-content rounded-box menu-sm z-[1] mt-3 w-52 bg-base-100 p-2 shadow"
-          >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
-        </div*/}
-        <a className="btn btn-ghost text-xl normal-case">Tasky</a>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li tabIndex={0}>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
-        </ul>
-      </div>
-      <div className="navbar-end z-10">
-        {/* <a className="btn">Button</a> */}
-        {session?.user ? (
-          <div className="dropdown-end dropdown">
-            <label tabIndex={0} className="avatar btn btn-circle btn-ghost">
-              <div className="w-10 rounded-full">
-                <img
-                  src="/images/stock/photo-1534528741775-53994a69daeb.jpg"
-                  alt="testing"
-                />
-              </div>
-            </label>
-            <ul
-              tabIndex={0}
-              className="menu dropdown-content rounded-box menu-sm z-[10] mt-3 w-52 bg-base-100 p-2 shadow"
-            >
-              <li>
-                <a className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </a>
-              </li>
-              <li>
-                <a>Settings</a>
-              </li>
-              <li onClick={handleLogout}>
-                <a>Logout</a>
-              </li>
-              <li>
-                <a>
-                  <span>Theme </span>
-                  <label className="swap swap-rotate ">
-                    {/* this hidden checkbox controls the state */}
-                    <input type="checkbox" onClick={handleThemeChange} />
-
-                    {theme === "fantasy" ? <Sun /> : <Moon />}
-                  </label>
-                </a>
-              </li>
-            </ul>
-          </div>
+            </DropdownTrigger>
+            <DropdownMenu aria-label="Profile Actions" variant="flat">
+              <DropdownItem key="profile" className="h-14 gap-2">
+                <p className="font-semibold">Signed in as</p>
+                <p className="font-semibold">{data.user.email}</p>
+              </DropdownItem>
+              <DropdownItem key="settings">My Settings</DropdownItem>
+              <DropdownItem key="team_settings">Team Settings</DropdownItem>
+              <DropdownItem key="analytics">Analytics</DropdownItem>
+              <DropdownItem key="system">System</DropdownItem>
+              <DropdownItem key="configurations">Configurations</DropdownItem>
+              <DropdownItem key="help_and_feedback">
+                Help & Feedback
+              </DropdownItem>
+              <DropdownItem key="logout" color="danger" onClick={handleSignOut}>
+                Log Out
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
         ) : (
           <LoginModal
             isOpen={isOpen}
@@ -204,9 +79,7 @@ const Navbar = () => {
             onOpenChange={onOpenChange}
           />
         )}
-      </div>
-    </div>
+      </NavbarContent>
+    </Navbar>
   );
-};
-
-export default Navbar;
+}
