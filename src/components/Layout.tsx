28cb6383b1a758b9b9d6b11208aa236d819a0127
Bhavesh Patil <96092183+Uraniumking007@users.noreply.github.com>
2023-09-21 09:51:53 +0530

    Default Font

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,7 @@ import CustomNavbar from "./Navbar";
 
 export default function Layout({ children }) {
   return (
-    <div className="h-screen w-screen">
+    <div className="h-screen w-screen font-nunito">
       <div className="z-50 flex h-fit w-full">
         <CustomNavbar />
       </div>
