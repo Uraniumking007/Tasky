97b19b69abe4958a469cd9ed50bd9c34e9597114
UraniumKing <bhaveshpatil918@gmail.com>
2023-09-25 03:20:24 +0530

    Basic Priorities

---
+++ src/pages/tasks/data.js
@@ -0,0 +1,18 @@
+export const PriorityCategories = [
+  {
+    label: "High Priority",
+    value: "High Priority",
+  },
+  {
+    label: "Medium Priority",
+    value: "Medium Priority",
+  },
+  {
+    label: "Low Priority",
+    value: "Low Priority",
+  },
+  {
+    label: "No Priority",
+    value: "No Priority",
+  },
+];
