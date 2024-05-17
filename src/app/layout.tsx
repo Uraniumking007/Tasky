4e4c7eea3acdef8b3b23a991a56189ca76670c37
UraniumKing <bhaveshpatil918@gmail.com>
2024-05-18 02:26:57 +0530

    chore: Update font in layout.tsx to use Nunito

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import "@/styles/globals.css";
-
-import { GeistSans } from "geist/font/sans";
+import { Nunito } from "next/font/google";
 
 import { TRPCReactProvider } from "@/trpc/react";
 
@@ -10,14 +9,20 @@ export const metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+const nunito = Nunito({
+  subsets: ["latin"],
+  display: "swap",
+  style: ["italic", "normal"],
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
-      <body>
+    <html lang="en" className={`${nunito.className}`}>
+      <body className="flex h-screen w-screen justify-center">
         <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
     </html>
