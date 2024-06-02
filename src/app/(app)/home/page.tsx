9e3931706bb931455e49b0236191948076fec38f
UraniumKing <bhaveshpatil918@gmail.com>
2024-06-02 11:56:25 +0530

    feat: Update HomePage styling
    Update the styling of the HomePage component to use a custom background color and padding. This improves the visual appearance and layout of the home page.

--- src/app/(app)/home/page.tsx
+++ src/app/(app)/home/page.tsx
@@ -1,6 +1,6 @@
 export default function HomePage() {
   return (
-    <div>
+    <div className="m-2 w-full rounded-sm bg-slate-800 p-4">
       <h1>Home Page</h1>
       <p>Welcome to the home page.</p>
     </div>
