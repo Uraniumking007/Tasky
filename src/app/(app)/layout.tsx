a1366acb4c488a225b8fe470d6bf97cb967b0a5a
UraniumKing <bhaveshpatil918@gmail.com>
2024-06-02 11:55:29 +0530

    feat: Add SideNavbar component to HomeLayout
    Add the SideNavbar component to the HomeLayout in order to display a side navigation menu for authenticated users. This improves the user experience by providing easy access to different sections of the application.

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,6 +1,30 @@
 import Navbar from "@/components/navbar";
+import SideNavbar from "@/components/side-navbar";
+import { getServerAuthSession } from "@/server/auth";
+import { User } from "next-auth";
 import { ReactNode } from "react";
 
-export default function HomeLayout({ children }: { children: ReactNode }) {
-  return <div className="w-full">{children}</div>;
+export default async function HomeLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const session = await getServerAuthSession();
+  const user = session?.user as User; // Cast the user object to the User type
+
+  if (!session?.user) {
+    return {
+      redirect: {
+        destination: "/auth/login",
+        permanent: false,
+      },
+    };
+  }
+
+  return (
+    <div className="flex w-full">
+      <SideNavbar user={user} />
+      {children}
+    </div>
+  );
 }
