0a4cecc48c710ea0b9dccbc83f9efb298fe6ceb1
UraniumKing <bhaveshpatil918@gmail.com>
2024-06-14 00:38:36 +0530

    feat: Add CreateTaskDialog component
    Add the CreateTaskDialog component to handle the creation of new tasks. This component provides a dialog interface with input fields for the task title and subtasks. It also includes validation for the task title and displays appropriate error messages. This addition improves the functionality and user experience of the application.

---
+++ src/components/create-task-modal.tsx
@@ -0,0 +1,187 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Task } from "@prisma/client";
+import { IconCirclePlus, IconEyePlus } from "@tabler/icons-react";
+import { Icon } from "lucide-react";
+import { useState } from "react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip";
+import { TaskData, createTask } from "@/app/(app)/tasks/action";
+import { TRPCClientError } from "@trpc/client";
+import { useToast } from "./ui/use-toast";
+
+export function CreateTaskDialog() {
+  const [task, setTask] = useState<TaskData>({
+    title: "",
+    content: "",
+    createdAt: new Date(),
+    status: "pending",
+  });
+  const [subTask, setSubTask] = useState<Partial<Task>[]>([]);
+  const [subTaskCount, setSubTaskCount] = useState("0");
+  const { toast } = useToast();
+  async function handleSubmit() {
+    if (task.title === "") {
+      toast({
+        variant: "destructive",
+        title: "Error",
+        description: "Task title cannot be empty",
+      });
+      return;
+    }
+    const newTask = {
+      ...task,
+      content: JSON.stringify(subTask),
+    };
+    try {
+      await createTask({ taskData: newTask });
+      toast({
+        variant: "default",
+        title: "Success",
+        description: "Task created successfully",
+      });
+    } catch (error) {
+      if (error) {
+        toast({
+          variant: "destructive",
+          title: "Error",
+          description: JSON.stringify(error),
+        });
+      }
+    }
+  }
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button
+          variant="secondary"
+          className="fixed bottom-10 right-10 h-16 w-16 rounded-full"
+        >
+          <IconCirclePlus size={258} className="h-full w-full" />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>New Task</DialogTitle>
+        </DialogHeader>
+
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-5 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Title
+            </Label>
+            <Input
+              id="task"
+              defaultValue=""
+              onChange={(e) => setTask({ ...task, title: e.target.value })}
+              className="col-span-3"
+            />
+            {subTaskCount === "0" && (
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger>
+                    <IconCirclePlus
+                      className="h-6 w-6"
+                      onClick={() => {
+                        setSubTaskCount(
+                          (parseInt(subTaskCount) + 1).toString(),
+                        );
+                        setSubTask([
+                          ...subTask,
+                          {
+                            id: "0",
+                            title: "",
+                            createdAt: new Date(),
+                            status: "pending",
+                          },
+                        ]);
+                      }}
+                    />
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>New SubTask</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            )}
+          </div>
+          {subTask.map((subtask, index) => (
+            <div className="grid grid-cols-5 items-center gap-2" key={index}>
+              <Label htmlFor="username" className="text-right">
+                {`SubTask ${index + 1}`}
+              </Label>
+              <Input
+                id="subtask"
+                className="col-span-3"
+                onChange={(e) => {
+                  const newSubTask = [...subTask];
+                  newSubTask.map((sub, i) => {
+                    if (i === index) {
+                      sub.title = e.target.value;
+                    }
+                  });
+                  setSubTask(newSubTask);
+                }}
+              />
+              {subTaskCount === (index + 1).toString() &&
+                (subTask ?? []).length > 0 && (
+                  <TooltipProvider>
+                    <Tooltip>
+                      <TooltipTrigger>
+                        <IconCirclePlus
+                          className="h-6 w-6"
+                          onClick={() => {
+                            setSubTaskCount(
+                              (parseInt(subTaskCount) + 1).toString(),
+                            );
+                            setSubTask([
+                              ...subTask,
+                              {
+                                id: "0",
+                                title: "",
+                                createdAt: new Date(),
+                                status: "pending",
+                              },
+                            ]);
+                          }}
+                        />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>New SubTask</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                )}
+            </div>
+          ))}
+        </div>
+        <DialogFooter>
+          <Button
+            type="submit"
+            onClick={() => {
+              handleSubmit();
+            }}
+          >
+            Save changes
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
