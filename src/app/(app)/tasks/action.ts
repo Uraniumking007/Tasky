8e95c4626cddc28eca61b9bf34bc69c51d185cee
UraniumKing <bhaveshpatil918@gmailcom>
2024-06-23 09:23:50 +0530

    feat: Add getAllTasks, getTaskById, updateTask, deleteTask, and changeTaskStatus functions
    Add the getAllTasks function to retrieve all tasks associated with the user's active team. This function checks for a valid session, finds the user in the database, and returns the tasks.
    
    Add the getTaskById function to retrieve a specific task by its ID. This function checks for a valid session, finds the user in the database, and returns the task.
    
    Add the updateTask function to update a task's data. This function checks for a valid session, finds the user in the database, and updates the task with the provided data.
    
    Add the deleteTask function to delete a task. This function checks for a valid session, finds the user in the database, and deletes the task.
    
    Add the changeTaskStatus function to update a task's status. This function checks for a valid session, finds the user in the database, and updates the task's status with the provided value.
    
    These additions enhance the functionality of the application by providing CRUD operations for tasks.

--- src/app/(app)/tasks/action.ts
+++ src/app/(app)/tasks/action.ts
@@ -2,7 +2,7 @@
 
 import { getServerAuthSession } from "@/server/auth";
 import { db } from "@/server/db";
-import { Task } from "@prisma/client";
+import { STATUS_CODES } from "http";
 
 export interface TaskData {
   title: string;
@@ -35,4 +35,161 @@ export async function createTask({ taskData }: { taskData: TaskData }) {
     console.error(error);
     return JSON.stringify(error);
   }
+}
+
+export async function getAllTasks() {
+  const session = await getServerAuthSession();
+
+  if (!session) {
+    throw new Error("No session found");
+  }
+
+  const user = await db.user.findUnique({
+    where: {
+      username: session.user.username,
+    },
+  });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  const data = await db.task.findMany({
+    where: {
+      teamId: user.active_team,
+    },
+  });
+  return {
+    data,
+  };
+}
+
+export async function getTaskById({ id }: { id: string }) {
+  const session = await getServerAuthSession();
+
+  if (!session) {
+    throw new Error("No session found");
+  }
+
+  const user = await db.user.findUnique({
+    where: {
+      username: session.user.username,
+    },
+  });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  const task = await db.task.findUnique({
+    where: {
+      id,
+      userId: user.id,
+    },
+  });
+
+  return task;
+}
+
+export async function updateTask({
+  id,
+  taskData,
+}: {
+  id: string;
+  taskData: TaskData;
+}) {
+  const session = await getServerAuthSession();
+
+  if (!session) {
+    throw new Error("No session found");
+  }
+
+  const user = await db.user.findUnique({
+    where: {
+      username: session.user.username,
+    },
+  });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  const task = await db.task.update({
+    where: {
+      id,
+      userId: user.id,
+    },
+    data: {
+      ...taskData,
+    },
+  });
+
+  return task;
+}
+
+export async function deleteTask({ id }: { id: string }) {
+  const session = await getServerAuthSession();
+
+  if (!session) {
+    throw new Error("No session found");
+  }
+
+  const user = await db.user.findUnique({
+    where: {
+      username: session.user.username,
+    },
+  });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  const task = await db.task.delete({
+    where: {
+      id,
+      userId: user.id,
+    },
+  });
+
+  return task;
+}
+
+export async function changeTaskStatus({
+  id,
+  status,
+}: {
+  id: string;
+  status: string;
+}) {
+  const session = await getServerAuthSession();
+
+  if (!session) {
+    throw new Error("No session found");
+  }
+
+  const user = await db.user.findUnique({
+    where: {
+      username: session.user.username,
+    },
+  });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  const task = await db.task.update({
+    where: {
+      id,
+      userId: user.id,
+    },
+    data: {
+      status,
+    },
+  });
+
+  return {
+    statusCode: 200,
+    data: task,
+    message: "Task Updated",
+  };
 }
