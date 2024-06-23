b4f962984f6503616502a64f284f29208d230959
UraniumKing <bhaveshpatil918@gmailcom>
2024-06-23 10:47:43 +0530

    Refactor: TasksPage component to improve code organization and readability. Move the getAllTasks function to a separate file for better separation of concerns. This change enhances maintainability and modularity of the codebase.

--- src/app/(app)/tasks/page.tsx
+++ src/app/(app)/tasks/page.tsx
@@ -1,16 +1,7 @@
 import { CreateTaskDialog } from "@/components/create-task-modal";
-import {
-  Card,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Checkbox } from "@/components/ui/checkbox";
-import { cn } from "@/lib/utils";
-import { getServerAuthSession } from "@/server/auth";
-import { db } from "@/server/db";
-import { redirect } from "next/navigation";
 import React from "react";
+import { getAllTasks } from "./action";
+import TasksHorizontalCard from "@/components/cards/tasks-horizontal-card";
 
 export default async function TasksPage() {
   const tasks = await getAllTasks();
@@ -18,52 +9,9 @@ export default async function TasksPage() {
     <div className="flex w-full flex-col gap-8 p-4">
       <h1>Tasks</h1>
 
-      <div className="flex w-full flex-col gap-4">
-        {tasks.data.map((task) => {
-          const subtasks = JSON.parse(task.content!);
-          const numberOfSubtasks = subtasks.length;
-          return (
-            <Card className={cn("flex w-full items-center px-4")}>
-              <Checkbox />
-              <CardHeader>
-                <CardTitle>{task.title}</CardTitle>
-                <CardDescription>
-                  {numberOfSubtasks === 0 ? "" : `+ ${numberOfSubtasks} Tasks`}
-                </CardDescription>
-              </CardHeader>
-            </Card>
-          );
-        })}
-      </div>
+      <TasksHorizontalCard tasks={tasks.data} />
 
       <CreateTaskDialog />
     </div>
   );
-}
-
-export async function getAllTasks() {
-  const session = await getServerAuthSession();
-
-  if (!session) {
-    redirect("/login");
-  }
-
-  const user = await db.user.findUnique({
-    where: {
-      username: session.user.username,
-    },
-  });
-
-  if (!user) {
-    redirect("/login");
-  }
-
-  const data = await db.task.findMany({
-    where: {
-      teamId: user.active_team,
-    },
-  });
-  return {
-    data,
-  };
 }
