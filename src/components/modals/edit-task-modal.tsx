869b31146978a20fd09993bb512e20540657be2a
UraniumKing007 <bhaveshpatil918@gmailcom>
2024-07-07 09:52:21 +0530

    refactor: Update addSubtask function to exclude unnecessary properties

--- src/components/modals/edit-task-modal.tsx
+++ src/components/modals/edit-task-modal.tsx
@@ -10,8 +10,14 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import type { Task } from "@prisma/client";
-import { IconCirclePlus } from "@tabler/icons-react";
+import type { SubTask, Task } from "@prisma/client";
+import {
+  IconCirclePlus,
+  IconDeviceFloppy,
+  IconTrash,
+  IconTrashFilled,
+  IconTrashX,
+} from "@tabler/icons-react";
 import { useRef, useState } from "react";
 import {
   Tooltip,
@@ -19,22 +25,38 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "../ui/tooltip";
-import { updateTask, type TaskData } from "@/app/(app)/tasks/action";
+import getSubtasksById, {
+  addSubtask,
+  deleteSubtask,
+  updateTask,
+  type TaskData,
+} from "@/app/(app)/tasks/action";
 import { useToast } from "../ui/use-toast";
 import { Pencil } from "lucide-react";
 import { DialogClose } from "@radix-ui/react-dialog";
 
-export function EditTaskModal({ task }: { task: Task }) {
+interface EditSubtaskType {
+  id: string;
+  title: string;
+  isSaved: boolean;
+}
+
+export function EditTaskModal({
+  task,
+  subtasks,
+}: {
+  task: Task;
+  subtasks: SubTask[];
+}) {
   const [editedTask, setEditedTask] = useState<TaskData>({
     ...task,
     content: task.content ?? "",
   });
-  // const [editedSubTask, editedSetSubTask] = useState<Partial<Task>[]>(
-  //   JSON.parse(task.content ?? "") as Partial<Task>[],
-  // );
-  // const [subTaskCount, setSubTaskCount] = useState(
-  //   editedSubTask.length.toString(),
-  // );
+  const [editedSubTasks, setEditedSubTasks] = useState<EditSubtaskType[]>(
+    subtasks.map((subtask) => ({ ...subtask, isSaved: true })),
+  );
+  const [subTaskCount, setSubTaskCount] = useState(subtasks.length);
+
   const { toast } = useToast();
   const ref = useRef<HTMLButtonElement>(null);
 
@@ -47,36 +69,105 @@ export function EditTaskModal({ task }: { task: Task }) {
       });
       return;
     }
-    const newTask = {
-      ...editedTask,
-      // content: JSON.stringify(editedSubTask),
-    };
     try {
-      await updateTask({ id: task.id, taskData: newTask });
+      await updateTask({ id: task.id, taskData: editedTask });
       toast({
         variant: "default",
         title: "Success",
         description: "Task Edited successfully",
       });
     } catch (error) {
-      if (error) {
-        toast({
-          variant: "destructive",
-          title: "Error",
-          description: JSON.stringify(error),
-        });
+      toast({
+        variant: "destructive",
+        title: "Error",
+        description: JSON.stringify(error),
+      });
+    }
+  }
+
+  const handleSubTaskChange = (index: number, title: string) => {
+    const newSubTasks = editedSubTasks.map((subtask, i) =>
+      i === index ? { ...subtask, title } : subtask,
+    );
+    setEditedSubTasks(newSubTasks);
+  };
+
+  const handleSaveSubTask = async (index: number) => {
+    const newSubTasks = editedSubTasks.map((subtask, i) =>
+      i === index ? { ...subtask, isSaved: true } : subtask,
+    );
+
+    for (const editedSubTask of editedSubTasks) {
+      if (!editedSubTask.title || editedSubTask.title == "") {
+        throw new Error("Subtask title cannot be empty");
+      }
+      if (!editedSubTask.isSaved) {
+        try {
+          await addSubtask({
+            subtask: {
+              title: editedSubTask.title,
+              taskId: task.id,
+              id: editedSubTask.id,
+              content: null,
+              status: "incomplete",
+              user_id: "",
+              createdAt: new Date(),
+              updatedAt: new Date(),
+            },
+            taskId: task.id,
+          });
+          toast({
+            variant: "default",
+            title: "Success",
+            description: "SubTask created successfully",
+          });
+        } catch (error) {
+          toast({
+            variant: "destructive",
+            title: "Error",
+            description: JSON.stringify(error),
+          });
+        }
       }
     }
+
+    setEditedSubTasks(newSubTasks);
+  };
+
+  const addNewSubTask = () => {
+    setEditedSubTasks([
+      ...editedSubTasks,
+      { id: crypto.randomUUID(), title: "", isSaved: false },
+    ]);
+    setSubTaskCount(subTaskCount + 1);
+  };
+
+  async function deleteSubTask(id: string) {
+    const newSubTasks = editedSubTasks.filter(
+      (subtask, index) => subtask.id !== id,
+    );
+    setSubTaskCount(subTaskCount - 1);
+    try {
+      await deleteSubtask({ id });
+      toast({
+        variant: "default",
+        title: "Success",
+        description: "SubTask deleted successfully",
+      });
+    } catch (error) {
+      toast({
+        variant: "destructive",
+        title: "Error",
+        description: JSON.stringify(error),
+      });
+    }
+    setEditedSubTasks(newSubTasks);
   }
 
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button
-          variant={"secondary"}
-          size={"icon"}
-          className="rounded-full p-2"
-        >
+        <Button variant="secondary" size="icon" className="rounded-full p-2">
           <Pencil size={32} />
         </Button>
       </DialogTrigger>
@@ -84,7 +175,6 @@ export function EditTaskModal({ task }: { task: Task }) {
         <DialogHeader>
           <DialogTitle>Edit Task</DialogTitle>
         </DialogHeader>
-
         <div className="grid gap-4 py-4">
           <div className="grid grid-cols-5 items-center gap-4">
             <Label htmlFor="name" className="text-right">
@@ -98,26 +188,13 @@ export function EditTaskModal({ task }: { task: Task }) {
               }
               className="col-span-3"
             />
-            {/* {subTaskCount === "0" && (
+            {subTaskCount === 0 && (
               <TooltipProvider>
                 <Tooltip>
                   <TooltipTrigger>
                     <IconCirclePlus
                       className="h-6 w-6"
-                      onClick={() => {
-                        setSubTaskCount(
-                          (parseInt(subTaskCount) + 1).toString(),
-                        );
-                        editedSetSubTask([
-                          ...editedSubTask,
-                          {
-                            id: "0",
-                            title: "",
-                            createdAt: new Date(),
-                            status: "pending",
-                          },
-                        ]);
-                      }}
+                      onClick={addNewSubTask}
                     />
                   </TooltipTrigger>
                   <TooltipContent>
@@ -125,58 +202,67 @@ export function EditTaskModal({ task }: { task: Task }) {
                   </TooltipContent>
                 </Tooltip>
               </TooltipProvider>
-            )} */}
+            )}
           </div>
-          {/* {editedSubTask.map((subtask, index) => (
-            <div className="grid grid-cols-5 items-center gap-2" key={index}>
-              <Label htmlFor="username" className="text-right">
-                {`SubTask ${index + 1}`}
-              </Label>
-              <Input
-                id="subtask"
-                className="col-span-3"
-                defaultValue={subtask.title ?? ""}
-                onChange={(e) => {
-                  const newSubTask = [...editedSubTask];
-                  newSubTask.map((sub, i) => {
-                    if (i === index) {
-                      sub.title = e.target.value;
-                    }
-                  });
-                  editedSetSubTask(newSubTask);
-                }}
-              />
-              {subTaskCount === (index + 1).toString() &&
-                (editedSubTask ?? []).length > 0 && (
+          {editedSubTasks.map((subtask, index) => {
+            return (
+              <div className="flex items-center gap-2" key={subtask.id}>
+                <Label htmlFor="subtask" className="text-nowrap text-right">
+                  {`SubTask ${index + 1}`}
+                </Label>
+                <Input
+                  id="subtask"
+                  className="col-span-3"
+                  defaultValue={subtask.title ?? ""}
+                  onChange={(e) => handleSubTaskChange(index, e.target.value)}
+                />
+                {editedSubTasks.length === index + 1 && (
+                  <TooltipProvider>
+                    {subtask.isSaved ? (
+                      <Tooltip>
+                        <TooltipTrigger asChild>
+                          <IconCirclePlus
+                            className="h-6 w-6"
+                            onClick={addNewSubTask}
+                          />
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          <p>New SubTask</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    ) : (
+                      <Tooltip>
+                        <TooltipTrigger asChild>
+                          <IconDeviceFloppy
+                            className="h-6 w-6"
+                            onClick={() => handleSaveSubTask(index)}
+                          />
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          <p>Save SubTask</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    )}
+                  </TooltipProvider>
+                )}
+                {subtask.isSaved && (
                   <TooltipProvider>
                     <Tooltip>
-                      <TooltipTrigger>
-                        <IconCirclePlus
+                      <TooltipTrigger asChild>
+                        <IconTrash
                           className="h-6 w-6"
-                          onClick={() => {
-                            setSubTaskCount(
-                              (parseInt(subTaskCount) + 1).toString(),
-                            );
-                            editedSetSubTask([
-                              ...editedSubTask,
-                              {
-                                id: subTaskCount.toString(),
-                                title: "",
-                                createdAt: new Date(),
-                                status: "pending",
-                              },
-                            ]);
-                          }}
+                          onClick={() => deleteSubTask(subtask.id)}
                         />
                       </TooltipTrigger>
                       <TooltipContent>
-                        <p>New SubTask</p>
+                        <p>Delete SubTask</p>
                       </TooltipContent>
                     </Tooltip>
                   </TooltipProvider>
                 )}
-            </div>
-          ))} */}
+              </div>
+            );
+          })}
         </div>
         <DialogFooter>
           <Button
