16493a661686eddb78eef8aea2eea6b22982af97
UraniumKing007 <bhaveshpatil918@gmailcom>
2024-07-07 08:44:33 +0530

    refactor: Add getAllSubTasks function to TasksPage code

--- src/app/(app)/tasks/page.tsx
+++ src/app/(app)/tasks/page.tsx
@@ -1,15 +1,16 @@
 import { TaskCreationModal } from "@/components/modals/create-task-modal";
 import React from "react";
-import { getAllTasks } from "./action";
+import { getAllSubTasks, getAllTasks } from "./action";
 import TasksHorizontalCard from "@/components/cards/tasks-horizontal-card";
 
 export default async function TasksPage() {
   const tasks = await getAllTasks();
+  const subTasks = await getAllSubTasks();
   return (
     <div className="flex w-full flex-col gap-8 p-4">
       <h1>Tasks</h1>
 
-      <TasksHorizontalCard tasks={tasks.data} />
+      <TasksHorizontalCard tasks={tasks.data} subTasks={subTasks.data} />
 
       <TaskCreationModal />
     </div>
