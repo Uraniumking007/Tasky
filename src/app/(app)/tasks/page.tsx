03a75ad9e98ec254828615bb6bee0a7e6be0b30d
UraniumKing007 <bhaveshpatil918@gmailcom>
2024-08-07 15:30:44 +0530

    refactor: Remove TaskCreationModal from TasksPage

--- src/app/(app)/tasks/page.tsx
+++ src/app/(app)/tasks/page.tsx
@@ -1,4 +1,3 @@
-import { TaskCreationModal } from "@/components/modals/create-task-modal";
 import React from "react";
 import { getAllSubTasks, getAllTasks } from "./action";
 import TasksHorizontalCard from "@/components/cards/tasks-horizontal-card";
@@ -9,10 +8,7 @@ export default async function TasksPage() {
   return (
     <div className="flex w-full flex-col gap-8 p-4">
       <h1>Tasks</h1>
-
       <TasksHorizontalCard tasks={tasks.data} subTasks={subTasks.data} />
-
-      <TaskCreationModal />
     </div>
   );
 }
