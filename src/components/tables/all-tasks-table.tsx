394b3bae8d272323bb1bcb37738b5aac241f5cca
UraniumKing007 <bhaveshpatil918@gmailcom>
2024-08-07 15:31:06 +0530

    refactor: Update AllTasksListTable to display subtask count with a plus sign

--- src/components/tables/all-tasks-table.tsx
+++ src/components/tables/all-tasks-table.tsx
@@ -1,13 +1,17 @@
+"use client";
+
 import {
   Table,
   TableBody,
   TableCell,
-  TableFooter,
   TableHead,
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
 import { SubTask, Task } from "@prisma/client";
+import { Checkbox } from "../ui/checkbox";
+import { updateTaskStatus } from "@/app/(app)/tasks/action";
+import { useToast } from "../ui/use-toast";
 
 export default function AllTasksListTable({
   tasks,
@@ -16,10 +20,39 @@ export default function AllTasksListTable({
   tasks: Task[];
   subtasks: SubTask[];
 }) {
+  const { toast } = useToast();
+
+  async function changeTaskStatusClient({
+    id,
+    status,
+  }: {
+    id: string;
+    status: string;
+  }) {
+    const result = await updateTaskStatus({
+      id,
+      status,
+    });
+    if (result.statusCode === 200) {
+      toast({
+        variant: "default",
+        description: "Task status updated",
+      });
+    } else {
+      toast({
+        variant: "destructive",
+        description: "Failed to update task status",
+      });
+    }
+  }
+
   return (
     <Table>
       <TableHeader>
         <TableRow>
+          <TableHead>
+            <Checkbox />
+          </TableHead>
           <TableHead>Task</TableHead>
           <TableHead>Sub Tasks</TableHead>
           <TableHead>Status</TableHead>
@@ -34,12 +67,25 @@ export default function AllTasksListTable({
           const subtaskCount = subtask.length;
           return (
             <TableRow key={task.id}>
+              <TableCell>
+                <Checkbox
+                  defaultChecked={task.status === "completed"}
+                  onCheckedChange={async (e) => {
+                    await changeTaskStatusClient({
+                      id: task.id,
+                      status: e ? "completed" : "pending",
+                    });
+                  }}
+                />
+              </TableCell>
               <TableCell className="font-medium">{task.title}</TableCell>
               <TableCell>
                 {subtaskCount === 0 ? "-" : `+${subtaskCount}`}
               </TableCell>
               <TableCell className="capitalize">{task.status}</TableCell>
-              <TableCell>{task.status}</TableCell>
+              <TableCell className="capitalize">
+                {task.priority.replace("-", " ")}
+              </TableCell>
             </TableRow>
           );
         })}
