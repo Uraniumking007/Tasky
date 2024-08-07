461b0c9ad4d43bebb8a9fa88d9505ce948b1e84c
UraniumKing007 <bhaveshpatil918@gmailcom>
2024-08-07 15:30:22 +0530

    refactor: Update home page layout to display tasks with no tasks found message

--- src/app/(app)/home/page.tsx
+++ src/app/(app)/home/page.tsx
@@ -14,10 +14,22 @@ export default async function HomePage() {
   const subTasks = await getAllSubTasks();
 
   return (
-    <div className="m-2 w-full rounded-sm  p-4">
+    <div className="m-2 flex w-full flex-col justify-center rounded-sm p-4">
       <h1 className="text-3xl font-black">Welcome Back</h1>
       <p className="font-normal">Here's List of All Your Tasks.</p>
-      <AllTasksListTable tasks={tasks.data} subtasks={subTasks.data} />
+      {tasks.data.length === 0 ? (
+        <div className="flex h-full w-full flex-col items-center justify-center">
+          <iframe
+            src="https://lottie.host/embed/c8b76e8b-c33e-4afb-b8b6-6493a02cc2c6/JpXj66hdGo.json"
+            className="h-3/5 w-3/5"
+          />
+          <p>No tasks found</p>
+        </div>
+      ) : (
+        <div className="h-full w-full">
+          <AllTasksListTable tasks={tasks.data} subtasks={subTasks.data} />
+        </div>
+      )}
     </div>
   );
 }
