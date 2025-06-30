064462746309cefc24dc22aa09d6b787cdbb29df
UraniumKing007 <bhaveshpatil8000@gmail.com>
2025-06-30 09:25:23 +0530

    refactor: Remove unused imports from post router for cleaner code

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,10 +1,4 @@
-import { z } from "zod";
-
-import {
-  createTRPCRouter,
-  protectedProcedure,
-  publicProcedure,
-} from "@/server/api/trpc";
+import { createTRPCRouter } from "@/server/api/trpc";
 
 export const postRouter = createTRPCRouter({
   // hello: publicProcedure
